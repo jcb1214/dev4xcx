079538fc5633cafb6b059797e1ce07fc1b4e8aa9
jcb <jcb911214@163.com>
2018-08-15 16:00:15 +0800

    修复因调整文件夹造成的bug

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -36,8 +36,7 @@ Page({
         } else if (this.data.canIUse) {
             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
             // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res =
-        >
+            app.userInfoReadyCallback = res =>
             {
                 this.setData({
                     userInfo: res.userInfo,
@@ -47,7 +46,7 @@ Page({
         } else {
             // 在没有 open-type=getUserInfo 版本的兼容处理
             wx.getUserInfo({
-                success: res = > {
+                success: res => {
                 app.globalData.userInfo = res.userInfo
             this.setData({
                 userInfo: res.userInfo,
